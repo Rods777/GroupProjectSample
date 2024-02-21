9b660307580476639dfd836cd1ebcbd304d359e2
QES FSE <QES FSE@DESKTOP-4SH7JA2>
2024-02-21 17:38:22 +0800

    Added new features

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,23 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
-		System.out.println("Hello");
-		System.out.println("Nandura ng pilay");
+		
 		
 		Scanner scan = new Scanner (System.in);
 		System.out.println("Bakla ba si Rodel?");
 		String name = scan.nextLine();
 		if (name.equals("yes") ||name.equals("oo")) {
-		   System.out.println("Totoo naman");
+		   System.out.println("Totoo naman talaga rodel wag mo na ikaila pa");
 		   System.out.println();
 		   
+		}
+		else if (name.equals("no") || name.equals("hindi")) {
+			System.out.println("Sino bang siraulo ang mag sasabi ng hindi kung totoo naman talaga na bakla ka?");
 		}
 		
 		else {
 			
-			System.out.println("Yes lang dapat");
+			System.out.println("Yes lang dapat ang isasagot wag yung puro ka katarantaduhan");
 		}
 		
 	}
